f6ec02fdc1c0f6afcd7c0aa4b9b68e97bd2e1266
Chloe Shepherd <chloe.g.shepherd@gmail.com>
2018-07-20 09:16:06 -0400

    Friday morning

--- Capstone.Web/Models/Survey.cs
+++ Capstone.Web/Models/Survey.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Capstone.Web.Models
 {
@@ -11,6 +12,12 @@ namespace Capstone.Web.Models
         public string Email { get; set; }
         public string State { get; set; }
         public string Activity { get; set; }
-
     }
+
+    public static List<SelectListItem> park = new List<SelectListItem>()
+        {
+            new SelectListItem() {Text = "Cuyahoga Valley National Park", Value = "CVNP"},
+            new SelectListItem() {Text = "Everglades National Park", Value = "ENP"}
+
+        };
 }
